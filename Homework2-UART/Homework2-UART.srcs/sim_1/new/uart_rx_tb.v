31ef0feef0c2d609eed28e4adbb02b2bd2e0bab8
TeddyElectronics <kyc20126@gmail.com>
2023-11-04 15:54:39 +0900

    rx_tb

--- Homework2-UART/Homework2-UART.srcs/sim_1/new/uart_rx_tb.v
+++ Homework2-UART/Homework2-UART.srcs/sim_1/new/uart_rx_tb.v
@@ -1,11 +1,10 @@
-module uart_tx_tb;
-
+module uart_rx_tb;
 reg clk;
 reg rst; 
 reg uart_tx_en; 
-reg [7:0] uart_tx_data; 
-wire tx_busy; 
-wire uart_txd;
+reg [7:0] uart_tx_data, uart_rx_data; 
+wire tx_busy, rx_busy;
+wire uart_d;
 
 always begin
     #5 clk = ~clk;
@@ -24,5 +23,6 @@ initial begin
         #1000000;
         $stop;
 end
-uart_tx tx0(clk,rst,uart_tx_en,uart_tx_data,tx_busy,uart_txd);
+uart_tx tx0(clk, rst, uart_tx_en, uart_tx_data, tx_busy, uart_d);
+uart_tx rx0(clk, rst, uart_d, rx_busy, uart_rx_data);
 endmodule
