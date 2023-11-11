8523348c69e5e1cd53a4e89e2623cb3175d8ea5f
TeddyElectronics <kyc20126@gmail.com>
2023-11-11 16:24:21 +0900

    HW3

---
+++ Homework3-DigitalClock/Homework3-DigitalClock.srcs/sources_1/new/clock.v
@@ -0,0 +1,9 @@
+module clock(
+input clk_6mhz,
+input rst,
+input clock_en,
+input digit,
+input up, down,
+output sec0, sec1, min0, min1, hrs0, hrs1
+    );
+endmodule
---
+++ Homework3-DigitalClock/Homework3-DigitalClock.srcs/sources_1/new/dec7.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/11/11 16:22:06
+// Design Name: 
+// Module Name: dec7
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dec7(
+
+    );
+endmodule
