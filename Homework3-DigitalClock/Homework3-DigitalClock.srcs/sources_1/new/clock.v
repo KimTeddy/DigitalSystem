bf030861385898b7e4bdd3d8658cdec95b1a9a47
TeddyElectronics <kyc20126@gmail.com>
2023-11-11 16:36:32 +0900

    clock

--- Homework3-DigitalClock/Homework3-DigitalClock.srcs/sources_1/new/clock.v
+++ Homework3-DigitalClock/Homework3-DigitalClock.srcs/sources_1/new/clock.v
@@ -4,6 +4,46 @@ input rst,
 input clock_en,
 input digit,
 input up, down,
-output sec0, sec1, min0, min1, hrs0, hrs1
+output reg sec0, sec1, min0, min1, hrs0, hrs1
     );
+
+assign sec0_ovf = (sec0 == 9) ? 1'b1 : 1'b0;
+assign sec1_ovf = (sec1 == 5) ? 1'b1 : 1'b0;
+assign min0_ovf = (min0 == 9) ? 1'b1 : 1'b0;
+assign min1_ovf = (min1 == 5) ? 1'b1 : 1'b0;
+assign hrs0_ovf = (hrs0 == 9) ? 1'b1 : 1'b0;
+assign hrs1_ovf = (hrs1 == 5) ? 1'b1 : 1'b0;
+
+always @(posedge clk_6mhz or posedge rst) begin//xx:xx:xs
+    if (rst)
+        sec0 <= 4'b0;
+    else if (clock_en) begin
+        if(sec0==9)sec0 <= 0;
+        else sec0 <= sec0 + 1;
+    end
+end
+always @(negedge sec0_ovf or posedge rst) begin//xx:xx:sx
+    if (rst)
+        sec1 <= 4'b0;
+    else if (clock_en) begin
+        if(sec1==5)sec1 <= 0;
+        else sec1 <= sec1 + 1;
+    end
+end
+always @(negedge sec1_ovf or posedge rst) begin//xx:xm:xx
+    if (rst)
+        min0 <= 4'b0;
+    else if (clock_en) begin
+        if(min0==9)min0 <= 0;
+        else min0 <= min0 + 1;
+    end
+end
+always @(negedge min0_ovf or posedge rst) begin//xx:mx:xx
+    if (rst)
+        min1 <= 4'b0;
+    else if (clock_en) begin
+        if(min1==5)min1 <= 0;
+        else min1 <= min1 + 1;
+    end
+end
 endmodule
